03cc27ff2463a14acf73de45092fc92dfc11571a
Podter <48936015+Podter@users.noreply.github.com>
2022-10-02 16:39:02 +0700

    Create Sites.svelte

---
+++ src/routes/Sites.svelte
@@ -0,0 +1,22 @@
+<button><a href="rick roll.mp4">Contact data</a></button>
+<!-- svelte-ignore a11y-missing-content -->
+<h1 />
+<button><a href="valve.mp4">free VALVe</a></button>
+<!-- svelte-ignore a11y-missing-content -->
+<h1 />
+<button><a href="https://wikipedia.org">Version 0.109.4</a></button>
+<!-- svelte-ignore a11y-missing-content -->
+<h1 />
+<button><a href="eng daniel.html">Amogus</a></button>
+<!-- svelte-ignore a11y-missing-content -->
+<h1 />
+<button><a href="eng site 2.html">SO INTERST SITE</a></button>
+<!-- svelte-ignore a11y-missing-content -->
+<h1 />
+<button><a href="eng memes.html">Memes</a></button>
+<!-- svelte-ignore a11y-missing-content -->
+<h1 />
+<button><a href="eng canvas game.html">test button</a></button>
+<!-- svelte-ignore a11y-missing-content -->
+<h1 />
+<button><a href="eng audio.html">audio test</a></button>
