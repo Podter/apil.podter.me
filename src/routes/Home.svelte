b4ff6c1c681723151be3b0dda6bfd11d43d91dae
Podter <48936015+Podter@users.noreply.github.com>
2022-10-02 16:32:04 +0700

    route

---
+++ src/routes/Home.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+  import strongApil from "../assets/strong_apil.png";
+  import junimoDance from "../assets/junimo_dance_unscreen.gif";
+  import apilcatWithDonut from "../assets/apilcat_with_donut.png";
+</script>
+
+<h1>Hello world!</h1>
+
+<h1>Apil amogusovich</h1>
+
+<p>Best apil ever</p>
+
+<h1>All loves apples</h1>
+
+<p><b>So importnt news</b></p>
+<ul>
+  <img src={strongApil} width="200" alt="strong apil" />
+  <li>saddfsad</li>
+  <li>asdfds</li>
+  <li>asdfgds</li>
+</ul>
+<h1>Breking news end</h1>
+
+<img src={junimoDance} width="300px" alt="junimo dance" />
+<img src={apilcatWithDonut} width="300px" alt="apilcat with donut" />
+
+<h2>Capyright 2090 apl softek</h2>
+
+<!-- <iframe
+  src="./eng all sites.html"
+  width="500px"
+  height="650px"
+  frameborder="0"
+  title="all sites"
+/> -->
+
+<br />
+
+<button><a href="https://podter.xyz">go back xd</a></button>
