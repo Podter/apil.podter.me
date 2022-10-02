d955c59045928e2aa4e9105d9f10e35fa1632043
Podter <48936015+Podter@users.noreply.github.com>
2022-10-02 16:39:07 +0700

    routes

--- src/App.svelte
+++ src/App.svelte
@@ -2,10 +2,12 @@
   import { Router, Route } from "svelte-routing";
 
   import Home from "./routes/Home.svelte";
+  import Sites from "./routes/Sites.svelte";
 
   export let url: string = "";
 </script>
 
 <Router {url}>
   <Route path="/" component={Home} />
+  <Route path="/sites" component={Sites} />
 </Router>
