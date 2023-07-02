6e58950af0984c636ea10e80c86ac6ca71e36c45
Nonthaphat Chaisu <48936015+Podter@users.noreply.github.com>
2023-07-02 18:25:45 +0700

    seo stuff

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,22 @@
 import { defineConfig } from "astro/config";
 
 import svelte from "@astrojs/svelte";
+import sitemap from "@astrojs/sitemap";
+import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [svelte()],
+  site: "http://localhost:3000",
+  integrations: [
+    svelte(),
+    sitemap(),
+    robotsTxt({
+      policy: [
+        {
+          userAgent: "*",
+          disallow: ["/"],
+        },
+      ],
+    }),
+  ],
 });
