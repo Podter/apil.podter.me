4d3e7bc99ee5cf5cba0b191379cde22430fa7cdc
Nonthaphat Chaisu <48936015+Podter@users.noreply.github.com>
2023-07-02 17:57:44 +0700

    better sites

---
+++ src/constants/pages.ts
@@ -0,0 +1,28 @@
+const pages: Record<string, { name: string }> = {
+  "/rickroll": {
+    name: "Contact data",
+  },
+  "/valve": {
+    name: "free VALVe",
+  },
+  "https://wikipedia.org": {
+    name: "Version 0.109.4",
+  },
+  "/daniel": {
+    name: "Amogus",
+  },
+  "/interst": {
+    name: "SO INTERST SITE",
+  },
+  "/memes": {
+    name: "Memes",
+  },
+  "/game": {
+    name: "test button",
+  },
+  "/audio": {
+    name: "audio test",
+  },
+};
+
+export default pages;
