65623ebd03b287bc9ad3e2ee6d93c21b499c8eb2
Nonthaphat Chaisu <48936015+Podter@users.noreply.github.com>
2024-04-23 19:33:47 +0700

    build and scripts update

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,6 @@ import robotsTxt from "astro-robots-txt";
 // https://astro.build/config
 export default defineConfig({
   site: "https://apil.podter.me",
+  output: "static",
   integrations: [svelte(), sitemap(), robotsTxt()],
 });
