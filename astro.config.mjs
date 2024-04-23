b33e7d3887a476ad87921f960d1560271098f72a
Nonthaphat Chaisu <48936015+Podter@users.noreply.github.com>
2024-04-23 19:28:04 +0700

    more integrations

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from "astro/config";
 
 import svelte from "@astrojs/svelte";
+import sitemap from "@astrojs/sitemap";
+import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [svelte()],
+  integrations: [svelte(), sitemap(), robotsTxt()],
 });
